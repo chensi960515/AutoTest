6923fdad871c2a378ed5b7c445d874a9e2a328b5
FLCL <909492683@qq.com>
2022-07-31 20:29:35 +0800

    重构请求,封装

---
+++ common/unit/checkResult.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/python3
+# -*- coding:utf-8 -*-
+"""
+@project: 
+@author: chensi
+@file: checkResult.py
+@time: 2022/7/31  20:18
+"""
+""" 校验接口返回 """
--- config/__init__.py
+++ config/__init__.py
@@ -3,3 +3,26 @@
 # @Author : chensi
 # @File : __init__.py.py
 # @Project : CVATest
+
+from common.utils.readYaml import read_yaml_data
+import os
+
+# 获取主目录路径
+ROOT_DIR = str(os.path.realpath(__file__)).split('config')[0].replace('\\', '/')
+
+# 获取配置文件路径
+API_CONFIG = ROOT_DIR + 'config/apiConfig_yaml'
+RUN_CONFIG = ROOT_DIR + 'config/runConfig_yaml'
+DB_CONFIG = ''
+
+# 获取运行配置信息
+RC = read_yaml_data(RUN_CONFIG)
+INTERVAL = RC['interval']
+PROJECT_NAME = RC['project_name']
+
+# 测试数据目录(test.yaml)
+PAGE_DIR = ROOT_DIR + PROJECT_NAME + '/page'
+# 测试脚本目录
+TEST_DIR = ROOT_DIR + PROJECT_NAME + '/testcase'
+# 测试报告目录
+REPORT_DIR = ROOT_DIR + PROJECT_NAME + '/report'
